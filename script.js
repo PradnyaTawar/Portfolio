bffd1949250dcf8f769658bdf36d843db4541b2f
PradnyaTawar <tawar.rani96@gmail.com>
2022-10-17 17:36:13 +0530

    progress percentages

--- script.js
+++ script.js
@@ -2,21 +2,40 @@ const navbar = document.querySelector(".navbar");
 const navbarOffsetTop = navbar.offsetTop;
 const sections = document.querySelectorAll("section");
 const navbarLinks = document.querySelectorAll(".navbar-link");
+const progress = document.querySelector(".progress-bars-wrapper");
+const progressBarPercents = [97, 89, 85, 87, 80, 70, 50];
 
 window.addEventListener("scroll", () => {
-    if(window.pageYOffset >= navbarOffsetTop){
-        navbar.classList.add("sticky");
-    }else{
-        navbar.classList.remove("sticky");
-    }
-    sections.forEach((section, i) => {
-        if (window.pageYOffset >= section.offsetTop - 10) {
-          navbarLinks.forEach((navbarLink) => {
-            navbarLink.classList.remove("change");
-          });
-          navbarLinks[i].classList.add("change");
-        }
+  mainFn();
+});
+
+const mainFn = () => {
+  if (window.pageYOffset >= navbarOffsetTop) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+
+  sections.forEach((section, i) => {
+    if (window.pageYOffset >= section.offsetTop - 10) {
+      navbarLinks.forEach((navbarLink) => {
+        navbarLink.classList.remove("change");
       });
-    
+      navbarLinks[i].classList.add("change");
+    }
   });
-  
+
+  if (window.pageYOffset + window.innerHeight >= progress.offsetTop) {
+    document.querySelectorAll(".progress-present ").forEach((el, i) => {
+      el.style.width = `${progressBarPercents[i]}%`;
+      el.previousElementSibling.firstElementChild.textContent =
+        progressBarPercents[i];
+    });
+  }
+};
+
+mainFn();
+
+window.addEventListener("resize", () => {
+  window.location.reload();
+});
