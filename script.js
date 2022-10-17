1b54ece01068c49cf6f933708046067fe6debb8a
PradnyaTawar <tawar.rani96@gmail.com>
2022-10-17 17:50:39 +0530

    commit

--- script.js
+++ script.js
@@ -6,36 +6,36 @@ const progress = document.querySelector(".progress-bars-wrapper");
 const progressBarPercents = [97, 89, 85, 87, 80, 70, 50];
 
 window.addEventListener("scroll", () => {
-  mainFn();
+    mainFn();
 });
 
 const mainFn = () => {
-  if (window.pageYOffset >= navbarOffsetTop) {
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
-  }
-
-  sections.forEach((section, i) => {
-    if (window.pageYOffset >= section.offsetTop - 10) {
-      navbarLinks.forEach((navbarLink) => {
-        navbarLink.classList.remove("change");
-      });
-      navbarLinks[i].classList.add("change");
+    if (window.pageYOffset >= navbarOffsetTop) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
     }
-  });
 
-  if (window.pageYOffset + window.innerHeight >= progress.offsetTop) {
-    document.querySelectorAll(".progress-present ").forEach((el, i) => {
-      el.style.width = `${progressBarPercents[i]}%`;
-      el.previousElementSibling.firstElementChild.textContent =
-        progressBarPercents[i];
+    sections.forEach((section, i) => {
+        if (window.pageYOffset >= section.offsetTop - 10) {
+            navbarLinks.forEach((navbarLink) => {
+                navbarLink.classList.remove("change");
+            });
+            navbarLinks[i].classList.add("change");
+        }
     });
-  }
+
+    if (window.pageYOffset + window.innerHeight >= progress.offsetTop) {
+        document.querySelectorAll(".progress-present ").forEach((el, i) => {
+            el.style.width = `${progressBarPercents[i]}%`;
+            el.previousElementSibling.firstElementChild.textContent =
+                progressBarPercents[i];
+        });
+    }
 };
 
 mainFn();
 
 window.addEventListener("resize", () => {
-  window.location.reload();
+    window.location.reload();
 });
